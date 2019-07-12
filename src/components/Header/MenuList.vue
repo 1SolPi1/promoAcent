31d43e3566f6da648c1f03f1bf82026d2660ed78
Solpi <rostikkozlenko61@gmail.com>
2019-07-13 00:12:03 +0300

    add new menulist

---
+++ src/components/Header/MenuList.vue
@@ -0,0 +1,46 @@
+<template>
+<ul>
+  <li
+    v-for="item in categories"
+    :key="item.id"
+    @click="selectCategory(item.id)"
+  >
+    <a href="javascript:void(0)">{{item.name}}</a>
+  </li>
+  <li class="mi_m"><a href="#">О проекте</a></li>
+  <li class="mi_m"><a href="#">Описание партнерства</a></li>
+</ul>
+</template>
+
+<script>
+	export default {
+    name: "MenuList",
+    components: {
+    },
+    props: {},
+    data() {
+			return {
+      }
+    },
+    created() {
+    },
+    mounted() {
+     },
+		methods: {
+      selectCategory(id){
+        this.$store.dispatch('changeSelectCategory', id);
+        this.$router.push('/questions/category/' + id);
+        this.$emit('close')
+      }
+		},
+		computed: {
+      categories(){
+        return this.$store.getters.CATEGORI
+      },
+    },
+	}
+</script>
+
+<style scoped>
+
+</style>
