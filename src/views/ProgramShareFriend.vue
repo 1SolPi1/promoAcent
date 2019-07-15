67ad49f84127ce1d0056e6ce0e0080b407e52909
Solpi <rostikkozlenko61@gmail.com>
2019-07-15 23:59:33 +0300

    add new component page

---
+++ src/views/ProgramShareFriend.vue
@@ -0,0 +1,39 @@
+<template>
+<div class="container">
+  <breadcrumbs
+        :wayPages="[
+          {
+            name: 'Главная',
+            route: '/',
+          }
+        ]"
+        currentPage="Условия программы 'Пригласи друга'"
+        />
+  <div class="title_section slideInUp wow" data-wow-iteration="1">
+    Условия программы "Пригласи друга"
+  </div>
+</div>
+</template>
+
+<script>
+export default {
+  name: "ProgramShareFriends",
+  components: {},
+  props: {},
+  data() {
+    return {}
+  },
+  created() {},
+  mounted() {
+    new WOW().init();
+  },
+  methods: {},
+  computed: {},
+}
+</script>
+
+<style scoped>
+.container{
+  height: calc(100vh - 237px);
+}  
+</style>
