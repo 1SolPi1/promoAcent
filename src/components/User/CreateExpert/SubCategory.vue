8749d932dadd406b3b5c5a0415b364777330af81
Solpi <rostikkozlenko61@gmail.com>
2019-07-17 00:51:22 +0300

    items

---
+++ src/components/User/CreateExpert/SubCategory.vue
@@ -0,0 +1,55 @@
+<template>
+	<select :id="'multi' + id"> 
+    <option value="0">Выберите подкатегорию</option>
+    <option 
+     v-for="item in childcategory"
+     :value="item.id"
+     :key="item.id"
+     :selected="item.id === id"
+     >
+     {{item.name}}
+   </option>
+ </select>
+</template>
+
+<script>
+	export default {
+    name: "SubCategory",
+    components: {
+    },
+    props: {
+      childcategory: Array,
+      id: Number
+    },
+    data() {
+			return {
+        count: 1
+      }
+    },
+    watch:{
+      childcategory(){
+        setTimeout(() =>{  
+         $('select').multiselect('rebuild');
+        }, 100) 
+      },
+    },
+    created() {
+    },
+    mounted() {
+      const vm = this
+       $('#multi' + this.id).multiselect({
+        onChange: function(element) {
+              vm.$emit('selectsub', element.val())
+            },
+      });
+     },
+		methods: {
+    
+		},
+		computed: {},
+	}
+</script>
+
+<style scoped>
+
+</style>
