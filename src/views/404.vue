4761528fdc9c71175fbbe412ec19e46247a12011
Solpi <rostikkozlenko61@gmail.com>
2019-08-31 16:43:18 +0300

    add 404

---
+++ src/views/404.vue
@@ -0,0 +1,30 @@
+<template>
+<div class="container">
+  <div class="title_section slideInUp wow" data-wow-iteration="1">
+    404
+  </div>
+</div>
+</template>
+
+<script>
+  export default {
+    name: "QuestionItem",
+    components: {},
+    props: {},
+    data() {
+      return {}
+    },
+    created() {},
+    mounted() {
+      new WOW().init();
+    },
+    methods: {},
+    computed: {},
+    }
+</script>
+
+<style scoped>
+.container{
+  height: calc(100vh - 237px);
+}    
+</style>
