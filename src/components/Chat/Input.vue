3306aa48f6846214d6a68cc7fc21bda1f21981cb
1SolPi1 <rostikkozlenko61@gmail.com>
2020-05-06 22:48:17 +0300

    add form review to chat

--- src/components/Chat/Input.vue
+++ src/components/Chat/Input.vue
@@ -20,6 +20,7 @@
     </svg> 
     cкрыть чаты
   </button>
+     <button type="button" class="btn btn-primary btn_chat" @click="reviewForms = !reviewForms">Оставить отзыв</button>
   <button 
     type="button" 
     class="btn btn-primary btn_chat" 
@@ -31,14 +32,23 @@
   </button>
   <button type="button" class="btn btn-primary btn_chat" @click="submitTextExpert()" v-else>Отправить</button>
 </div>
+
+      <reviewForm
+              v-if="reviewForms"
+              :id="activechat[0].expert_id"
+              @close="reviewForms = !reviewForms"
+              :needClose="false"
+      />
 </div>
 </template>
 
 <script>
   import EventBus from '@/scripts/eventBus';
+  import reviewForm from '@/components/Expert/Reviews/FormReview'
 	export default {
     name: "Input",
     components: {
+        reviewForm
     },
     props: {
       activechat:{
@@ -46,8 +56,9 @@
       }
     },
     data() {
-			return {
-        newMessage: null
+        return {
+            newMessage: null,
+            reviewForms: false,
       }
     },
     created() {
